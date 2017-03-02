a2f07b8f96060eebe2394930eda007d6845db4c8
Gilles Vieira <gilles.vieira@dailymotion.com>
2017-03-02 11:05:55 +0100

    Add option do store files Switch to perceptual img compare (much much slower)

--- main.go
+++ main.go
@@ -3,17 +3,59 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
 	"github.com/crhym3/imgdiff"
 	"github.com/opennota/screengen"
-	"image"
-	"os"
 )
 
 var (
+	verbose       = flag.Bool("v", false, "Verbose")
+	keepFiles     = flag.Bool("k", false, "Keep files in png format")
 	n             = flag.Int("n", 5, "Number of images to compare")
 	maxSameImg    = flag.Int("s", 2, "Maximal number of times 2 consecutive images can be similar")
 	diffThreshold = 10.0
 )
+
+func mkdir(name string) (string, error) {
+	base := name
+	for i := 0; ; i++ {
+		_, err := os.Stat(name)
+		if os.IsNotExist(err) {
+			break
+		}
+		name = base + fmt.Sprintf("_%d", i)
+	}
+	return name, os.Mkdir(name, 0755)
+}
+
+func expand(tmpl, filename string) string {
+	name := path.Base(filename)
+	ext := path.Ext(name)
+	name = name[:len(name)-len(ext)]
+	return strings.Replace(tmpl, "%n", name, -1)
+}
+
+// writeImage writes image img to the file fn.
+func writeImage(img image.Image, fn string) {
+	f, err := os.OpenFile(fn, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't create file: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, img)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "PNG encoding error: %v\n", err)
+		os.Exit(1)
+	}
+}
 
 func detectSpam(fn string) {
 	gen, err := screengen.NewGenerator(fn)
@@ -23,15 +65,29 @@ func detectSpam(fn string) {
 	}
 	defer gen.Close()
 
+	differ := imgdiff.NewPerceptual(2.2, 100.0, 45.0, 1.0, true)
+
+	// Fast seek mode
 	gen.Fast = true
 
 	inc := gen.Duration / int64(*n)
-	differ := imgdiff.NewBinary()
 
 	var (
 		imgCur  image.Image
 		imgPrev image.Image
+		dname   string
 	)
+
+	dirtmpl := "%n"
+
+	if *keepFiles {
+		dname = expand(dirtmpl, fn)
+		dname, err = mkdir(dname)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Can't create directory: %v\n", err)
+			os.Exit(1)
+		}
+	}
 
 	imgCur = nil
 	imgPrev = nil
@@ -47,6 +103,12 @@ func detectSpam(fn string) {
 			os.Exit(1)
 		}
 
+		if *keepFiles {
+			fntmpl := "shot%03d.png"
+			fn := filepath.Join(dname, fmt.Sprintf(fntmpl, i))
+			fmt.Printf("Writting %s to disk\n", fn)
+			writeImage(imgCur, fn)
+		}
 		if imgPrev != nil && imgCur != nil {
 			res, n, err := differ.Compare(imgCur, imgPrev)
 			if err != nil {
@@ -58,8 +120,9 @@ func detectSpam(fn string) {
 			if np <= diffThreshold {
 				identicalImgCount++
 			}
-
-			//			fmt.Printf("difference: %f%%\n", np)
+			if *verbose {
+				fmt.Printf("difference: %f%%\n", np)
+			}
 		}
 		d += inc
 	}
